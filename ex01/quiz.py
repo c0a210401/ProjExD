d2696425156c39a2bc62e8bc555146eb22e6f7cd
k.k2_kd <c0a210401a@edu.teu.ac.jp>
2022-06-14 15:47:22 +0900

    サザエさんクイズ

---
+++ ex01/quiz.py
@@ -0,0 +1,24 @@
+from random import randint
+def main():
+    print("問題:")
+    mon = shutudai()
+    kaitou(mon)
+
+def shutudai():
+    q = {}
+    q[0] = ("サザエの旦那の名前は？", "マスオ", "ますお")
+    q[1] = ("カツオの妹の名前は？", "ワカメ", "わかめ")
+    q[2] = ("タラオはカツオから見てどんな関係？", "甥", "おい", "甥っ子", "おいっこ")
+    s = randint(0, 2)
+    print(q[s][0])
+    return q[s]
+
+def kaitou(mon):
+    kai = input("答えるんだ:")
+    if kai in mon:
+        print("正解!!!")
+    else:
+        print("出直してこい")
+
+if __name__ ==  "__main__":
+    main()
