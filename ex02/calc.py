bc3308a8ff00305ee69b922e3df656ccc54aaf92
k.k2_kd <c0a210401a@edu.teu.ac.jp>
2022-06-21 15:08:06 +0900

    問7

--- ex02/calc.py
+++ ex02/calc.py
@@ -42,7 +42,7 @@ if __name__ ==  "__main__":
     entry = tk.Entry(justify=tk.RIGHT, width=10, font=("Times New Roman", 40))
     entry.grid(row=0, column=0, columnspan=3)
 
-    button_plus = tk.Button(root, text="+", font=("Times New Roman", 30), height=2, width=4, command=Plus)
+    button_plus = tk.Button(root, text="+", font=("Times New Roman", 30), height=2, width=4)
     button_plus.grid(row=4, column=1)
     button_plus.bind("<1>", Plus)
 
