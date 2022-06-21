9062846f58e724b12200a3076bc9e89420ff0073
k.k2_kd <c0a210401a@edu.teu.ac.jp>
2022-06-21 14:36:52 +0900

    問3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,16 +1,24 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
 if __name__ ==  "__main__":
     root = tk.Tk()
     root.title("tk")
     root.geometry("300x500")
 
+    def button_click(event):
+        btn = event.widget
+        txt = btn["text"]
+        tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
+    
     r,c = 0,0
     for text in range(9, -1, -1):
         btn = tk.Button(root, text = f"{text}", height=2, width=4, font=("Times New Roman", 30))
+        btn.bind("<1>", button_click)
         btn.grid(row=r, column=c)
         c += 1
         if (text -1)%3 == 0:
             r += 1
             c = 0
-            
+
     root.mainloop()
