eb441541cfe38febb7c46b8083a1ab92eeaa3dc2
k.k2_kd <c0a210401a@edu.teu.ac.jp>
2022-06-21 14:34:15 +0900

    問1

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-
-root = tk.Tk()
-root.title("tk")
-root.geometry("300x500")
-root.mainloop()
+if __name__ ==  "__main__":
+    root = tk.Tk()
+    root.title("tk")
+    root.geometry("300x500")
+    root.mainloop()
