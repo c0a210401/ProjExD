f61cb0ca887bd6a76b8e3997a77eadd5855f0204
k.k2_kd <c0a210401a@edu.teu.ac.jp>
2022-06-21 14:53:48 +0900

    問7

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,6 +10,24 @@ if __name__ ==  "__main__":
         txt = btn["text"]
         entry.insert(tk.END, txt)
         tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
+
+    def Plus(event):
+        btn = event.widget
+        txt = btn["text"]
+        form = entry.get()
+        if "+" in form[-1]:
+            tkm.showinfo("エラー", f"記号は連続できません")
+        else:
+            entry.insert(tk.END, txt)
+            tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
+
+    def Equal(event):
+        btn = event.widget
+        txt = btn["text"]
+        ans = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, ans)
+        tkm.showinfo("結果", f"[{txt}]ボタンが押されました 計算完了")
     
     r,c = 1,0
     for text in range(9, -1, -1):
@@ -24,8 +42,12 @@ if __name__ ==  "__main__":
     entry = tk.Entry(justify=tk.RIGHT, width=10, font=("Times New Roman", 40))
     entry.grid(row=0, column=0, columnspan=3)
 
-    button_plus = tk.Button(root, text="+", font=("Times New Roman", 30), height=2, width=4, command=button_click)
+    button_plus = tk.Button(root, text="+", font=("Times New Roman", 30), height=2, width=4, command=Plus)
     button_plus.grid(row=4, column=1)
-    button_plus.bind("<1>", button_click)
+    button_plus.bind("<1>", Plus)
+
+    button_equ = tk.Button(root, text="=", font=("Times New Roman", 30), height=2, width=4, command=Equal)
+    button_equ.grid(row=4, column=2)
+    button_equ.bind("<1>", Equal)
 
     root.mainloop()
