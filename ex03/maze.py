0d339d6c9131137246940d6acd5f46f7a59ac899
k.k2_kd <c0a210401a@edu.teu.ac.jp>
2022-06-28 14:15:55 +0900

    迷路1

---
+++ ex03/maze.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    label = tk.Label(root,
+            font=("Times New Roman", 80)
+            )
+    label.pack()
+    root.mainloop()
