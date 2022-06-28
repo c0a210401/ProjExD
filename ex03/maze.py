3d41d6c053a36d15c548ee3ee1ebc3718fca0060
k.k2_kd <c0a210401a@edu.teu.ac.jp>
2022-06-28 16:59:10 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,7 +10,7 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy, mx, my
+    global cx, cy, mx, my, tori
     mas_d = {
         ""     : [0, 0],
         "Up"   : [0, -1],
@@ -24,8 +24,24 @@ def main_proc():
     except:
         pass
     cx, cy = mx*100+50, my*100+50
+    bunsin()
+    if my == 7 and mx == 13:
+        tori = tk.PhotoImage(file="fig/6.png")
+        canvas.create_image(cx, cy, image=tori, tag="tori")
     canvas.coords("tori", cx, cy)
     root.after(100, main_proc)
+
+def sandg():
+    global cx, cy
+    canvas.create_rectangle(100, 100, 100+100, 100+100, 
+            fill="blue")
+
+    canvas.create_rectangle(1300, 700, 1300+100, 700+100, 
+            fill="red")
+
+def bunsin():
+    sandg()
+    canvas.create_image(cx, cy, image=tori, tag="tori")
 
 if __name__ == "__main__":
     root = tk.Tk()
